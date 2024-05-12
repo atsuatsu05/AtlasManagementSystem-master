5d88d9f76bb6bc0f548b983df3b8dfa3d8c00244
あつみ <>
2024-05-13 00:10:38 +0900

    環境構築 #3394　完了しました。

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            [
+                'over_name' => '田中',
+                'under_name' => '花子',
+                'over_name_kana' => 'タナカ',
+                'under_name_kana' => 'ハナコ',
+                'mail_address' => 'tanaka@gmail.com',
+                'sex' => '2',
+                'birth_day' => '2000/01/01',
+                'role' => '1',
+                'password' => bcrypt('tanaka123'),
+            ]
+            ]);
     }
 }
