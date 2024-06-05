07d4cf9aaa7133f510bcde05b621c8bf21782791
あつみ <>
2024-06-05 19:46:35 +0900

    機能追加（スクール予約） #3408　コミットし忘れておりました。申し訳ありません。

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,3 +1,18 @@
-$(function(){
+$(function () {
+  $('.reserve-modal-open').on('click', function () {
+    $('.js-modal').fadeIn();
+    var reserve_date = $(this).attr('value');
+    var reserve_part = $(this).attr('reservePart');
+    var setting_part = $(this).attr('settingPart');
+    $('.modal-inner-date').text('予約日：' + reserve_date);
+    $('.date-modal-hidden input').val(reserve_date);
+    $('.modal-inner-part').text('時間：' + reserve_part);
+    $('.part-modal-hidden input').val(setting_part);
+    return false;
+  });
+  $('.js-modal-close').on('click', function () {
+    $('.js-modal').fadeOut();
+    return false;
+  });
 
 });
